3bb084f32c861fa31623f284ccfe42e80e2578de
Steven Smodish <smodish@hotmail.com>
2020-11-19 12:27:21 -0500

    Removed join table for one to many relationship

--- migrations/20201020152845_create-service-table.js
+++ migrations/20201020152845_create-service-table.js
@@ -3,6 +3,10 @@ exports.up = function (knex) {
     tbl.increments()
     tbl.string('service').unique().notNullable()
     tbl.decimal('price', 2)
+    tbl
+    .integer('serviceProvider_id')
+    .references('id')
+    .inTable('service-provider')
   })
 }
 
